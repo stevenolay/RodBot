00b6b85a8ab74caacabfdf0c2560dc46b7575587
Steven O'Brian Layne <sol307@maclab-b182-24.library.northwestern.edu>
2016-04-09 20:47:29 -0500

    added categorical flags

--- rodbot/rodbot.py
+++ rodbot/rodbot.py
@@ -3,22 +3,26 @@ sys.path.append("requests-2.9.1-py2.7.egg")
 import requests
 import random
 
+happy = ['cute', 'lol', 'yaaass', 'omg']
+funny = ['lol', 'fail']
+sad = ['fail', 'omg']
 
-r = requests.get('http://www.buzzfeed.com/api/v2/feeds/lol')
-res = r.json()
-buzzes = res['buzzes']
-print len(buzzes)
-buzzes = []
-print len(buzzes)
-for i in range(1,10):
-    r = requests.get('http://www.buzzfeed.com/api/v2/feeds/lol?p='+str(i))
+def findArticle(adj): #Provide string such as 'funny', 'happy', 'sad'
+    r = requests.get('http://www.buzzfeed.com/api/v2/feeds/lol')
     res = r.json()
-    buzz = res['buzzes']
-    for j in buzz:
-        buzzes.append(j)
-        
-num = random.randint(1, 100)
+    buzzes = res['buzzes']
+    print len(buzzes)
+    buzzes = []
+    print len(buzzes)
+    for i in range(1,10):
+        r = requests.get('http://www.buzzfeed.com/api/v2/feeds/lol?p='+str(i))
+        res = r.json()
+        buzz = res['buzzes']
+        for j in buzz:
+            buzzes.append(j)
+            
+    num = random.randint(1, 100)
 
-print 'http://www.buzzfeed.com/' + buzzes[num]['username'] + "/" + buzzes[num]['uri']
-    
-    
+    print 'http://www.buzzfeed.com/' + buzzes[num]['username'] + "/" + buzzes[num]['uri']
+        
+        
