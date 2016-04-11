9921fdb1658e36cac47eaa7b7f0441b9d9906343
corpulentgowk <stevenlayne2017@u.northwestern.edu>
2016-04-11 00:09:08 -0500

    updated rodbot base article selection protocol

--- rodbot.py
+++ rodbot.py
@@ -8,21 +8,21 @@ funny = ['lol', 'fail']
 sad = ['fail', 'omg']
 
 def findArticle(adj): #Provide string such as 'funny', 'happy', 'sad'
-    r = requests.get('http://www.buzzfeed.com/api/v2/feeds/lol')
-    res = r.json()
-    buzzes = res['buzzes']
-    print len(buzzes)
-    buzzes = []
-    print len(buzzes)
-    for i in range(1,10):
-        r = requests.get('http://www.buzzfeed.com/api/v2/feeds/lol?p='+str(i))
-        res = r.json()
-        buzz = res['buzzes']
-        for j in buzz:
-            buzzes.append(j)
-            
-    num = random.randint(1, 100)
-
-    print 'http://www.buzzfeed.com/' + buzzes[num]['username'] + "/" + buzzes[num]['uri']
+	if str(adj) == "happy":
+		adj = random.choice(happy)
+	if str(adj) == "funny":
+		adj = random.choice(happy)
+	if str(adj) == "sad":
+		adj = random.choice(happy)
+	buzzes = []
+	for i in range(1,10):
+		r = requests.get('http://www.buzzfeed.com/api/v2/feeds/'+str(adj)+'?p='+str(i))
+		res = r.json()
+		buzz = res['buzzes']
+		for j in buzz:
+			buzzes.append(j)
+	if buzzes:	
+		num = random.randint(0, len(buzzes) - 1)
+		print 'http://www.buzzfeed.com/' + buzzes[num]['username'] + "/" + buzzes[num]['uri']
         
-        
+findArticle('funny')        
