4fb38b2df13e63b8921edafad244328f443293d1
corpulentgowk <stevenlayne2017@u.northwestern.edu>
2016-04-16 01:04:17 -0500

    Added summary to articles/added chat flow

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -9,6 +9,8 @@ from flask import Flask, request, session, g, redirect, url_for, \
 from jinja2 import Environment, FileSystemLoader
 import os
 
+from Summary import *
+import re, urllib2
 
 ##Chat Bot/BUZZFEED API IMPORTS
 import requests
@@ -28,21 +30,23 @@ app.config.from_object(__name__)
 #def connect_db():
     #return sqlite3.connect(app.config['DATABASE'])
 status =  True
+
 @app.route('/')
 def show_vanilla_home():
 	return render_template('index.html', data = {'status': '', 'article_url': ''})
 
-@app.route('/<param>')
+@app.route('/find/<param>')
 def show_entries(param):
     #entries = [dict(title='hello', text='swaggy')]
 	art = findArticle(str(param))
-	return render_template('index.html', data = {'status': 'submitted', 'article_url': art})
+	return art
 	
-@app.route('/status')	
-def get_status():
-	return str(status)
-	
+def summarize(title, content):
 
+	st = SummaryTool()
+	sentences_dic = st.get_sentences_ranks(content)
+	summary = st.get_summary(title, content, sentences_dic)
+	return jsonify(result=summary)
 
 happy = ['cute', 'lol', 'yaaass', 'omg']
 funny = ['lol', 'fail']
@@ -52,9 +56,9 @@ def findArticle(adj): #Provide string such as 'funny', 'happy', 'sad'
 	if str(adj) == "happy":
 		adj = random.choice(happy)
 	if str(adj) == "funny":
-		adj = random.choice(happy)
+		adj = random.choice(funny)
 	if str(adj) == "sad":
-		adj = random.choice(happy)
+		adj = random.choice(sad)
 	buzzes = []
 	for i in range(1,10):
 		r = requests.get('http://www.buzzfeed.com/api/v2/feeds/'+str(adj)+'?p='+str(i))
@@ -64,7 +68,22 @@ def findArticle(adj): #Provide string such as 'funny', 'happy', 'sad'
 			buzzes.append(j)
 	if buzzes:	
 		num = random.randint(0, len(buzzes) - 1)
-		return 'http://www.buzzfeed.com/' + buzzes[num]['username'] + "/" + buzzes[num]['uri']
+		url = "http://www.buzzfeed.com/api/v2/buzz/" + str(buzzes[num]['id'])
+		r = requests.get(url) 
+		resp = r.json()
+		resp = resp['buzz']['sub_buzzes']
+		content = ""
+		for i in resp:
+			content += i['description']
+		title = buzzes[num]['title']
+		title = urllib2.unquote(title)
+		content = urllib2.unquote(content)
+		st = SummaryTool()
+		sentences_dic = st.get_sentences_ranks(content)
+		summary = st.get_summary(title, content, sentences_dic)
+		
+		#return 'http://www.buzzfeed.com/' + buzzes[num]['username'] + "/" + buzzes[num]['uri'] + content + "\n" +  buzzes[num]['id']
+		return "Summary: " + summary + "\n" + "Content Original: " + content + "Title: " + title
 	return " "
 	
 if __name__ == '__main__':
