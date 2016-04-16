f46e3e2ec223d444871300b39b431623bf07e6cc
corpulentgowk <stevenlayne2017@u.northwestern.edu>
2016-04-16 01:13:07 -0500

    Added chat flow to JS

--- flaskr/static/main.js
+++ flaskr/static/main.js
@@ -1,6 +1,38 @@
+
+
 function findArticle(args){
 
 	var adj = document.getElementById("chat").value
 	console.log(adj)
 	window.location = adj
+}
+
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
+/*
+
+*/
+
+function findArticle2(){
+	linebreak = document.createElement("br");
+	var nar= document.getElementById("narrator");
+
+	var formattingU = ">: ";
+	var formattingN = "[]: ";
+	var userChat = document.getElementById("chat").value;
+	nar.appendChild(linebreak);
+	var url = "http://localhost:5000/find/" + userChat;
+	var buzzURL	= httpGet(url);
+
+
+	nar.innerHTML += formattingU + userChat;
+	nar.appendChild(linebreak);
+	nar.innerHTML += formattingN + buzzURL;
+	//console.log(userChat);
+	
 }
