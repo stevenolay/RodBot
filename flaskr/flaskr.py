5ff51b4eec3245751b04f502964060072184568f
Steven Layne <stevenlayne2017@u.northwestern.edu>
2016-05-02 23:17:07 +0000

    Added asynchronous callbacks

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -94,4 +94,4 @@ def findArticle(adj): #Provide string such as 'funny', 'happy', 'sad'
 	return " "
 	
 if __name__ == '__main__':
-    app.run()
+    app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
